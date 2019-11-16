544cbbaa40c1159ca5bbacde920d41bdebecac91
catherineuhs <cliffordcat@urbandaleschools.com>
2019-11-16 15:13:21 -0600

    new change

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -7,7 +7,7 @@ Sign your name:________________
 2.) Convert Binary 11101001 to Octal.
 351
 3.) Convert Hexadecimal FC to Binary.
-11111
+11111100
 4.) Convert Hexadecimal 1F to Decimal.
 31
 5.) Convert Hexadecimal #FAAFBD to RGB.
